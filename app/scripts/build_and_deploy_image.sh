462ec46bec476ba4fc6486d830771f8a711076e1
sisisin <azsisisin@gmail.com>
2024-11-10 16:23:26 +0900

    chore: build only flag

--- app/scripts/build_and_deploy_image.sh
+++ app/scripts/build_and_deploy_image.sh
@@ -5,6 +5,31 @@ set -ef -o pipefail
 script_dir=$(cd "$(dirname "$0")" && pwd)
 readonly script_dir
 
+mode=""
+for i in "$@"; do
+    case "$i" in
+    --only-build*)
+        mode="only-build"
+        shift
+        ;;
+    --*= | --*)
+        echo "Error: Unsupported flag $1" >&2
+        exit 1
+        ;;
+    *)
+        echo "Error: Unsupported flag $1" >&2
+        exit 1
+        ;;
+    esac
+done
+
+should_build=true
+should_deploy=true
+
+if [ "$mode" = "only-build" ]; then
+    should_deploy=false
+fi
+
 cd "$script_dir/.."
 
 ts=$(TZ=JST-9 date "+%Y%m%d-%H%M%S")
@@ -12,13 +37,19 @@ image_name=$(pulumi --cwd="${script_dir}/../../infra" stack output imageName)
 registry_domain=$(pulumi --cwd="${script_dir}/../../infra" stack output registryDomain)
 image_id=${image_name}:$ts
 
-docker build --platform linux/amd64 -t "$image_id" .
-gcloud auth configure-docker "${registry_domain}"
-docker push "$image_id"
+if [ "$should_build" = true ]; then
+    echo "Building image..."
 
-echo "Done."
-echo "$image_id"
+    docker build --platform linux/amd64 -t "$image_id" .
 
-sed -i '' "s|image: .*|image: $image_id|" job.yaml
+    echo "Image built: $image_id"
+fi
 
-gcloud run jobs replace job.yaml
+if [ "$should_deploy" = true ]; then
+    gcloud auth configure-docker "${registry_domain}"
+    docker push "$image_id"
+
+    sed -i '' "s|image: .*|image: $image_id|" job.yaml
+
+    gcloud run jobs replace job.yaml
+fi
