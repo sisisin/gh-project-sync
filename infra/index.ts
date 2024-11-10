1726868a6a552a2a708a80d13e9a2f7100ad4e89
sisisin <azsisisin@gmail.com>
2024-11-10 20:56:16 +0900

    rename run name

--- infra/index.ts
+++ infra/index.ts
@@ -9,3 +9,4 @@ export const bucketNameForGithubProjectSync = outputs.bucketName;
 export const imageName = outputs.imageName;
 export const registryDomain = outputs.registryDomain;
 export const region = gcpConfig.require('region');
+export const runName = outputs.runName;
